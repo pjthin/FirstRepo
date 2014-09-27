a06567836e14888ec363da7a26afef98137d8d01
pjthin <pierrejean.thin@gmail.com>
2014-09-27 16:04:20 +0200

    Commit 1

--- root/src/main/java/fr/pjthin/root/App.java
+++ root/src/main/java/fr/pjthin/root/App.java
@@ -4,10 +4,9 @@ package fr.pjthin.root;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("This is my first code line. :-) ");
     }
 }
