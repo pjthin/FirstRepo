885e0130d946fb3ab91170830b53aac755459c4e
Pierre-Jean.Thin <Pierre-Jean.Thin@LIN0108238.CMB.PLATINE.ORG>
2017-05-16 14:47:29 +0200

    add lombok and some plugin in pom to use lombok and selma (as they both are post compile processor)

--- root/src/main/java/fr/pjthin/root/selma/UserBo.java
+++ root/src/main/java/fr/pjthin/root/selma/UserBo.java
@@ -2,59 +2,18 @@ package fr.pjthin.root.selma;
 
 import java.util.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserBo {
-	@Override
-	public String toString() {
-		return "UserBo [id=" + id + ", login=" + login + ", created=" + created + ", lastConnection=" + lastConnection
-				+ "]";
-	}
 
-	int id;
-	String login;
-	Date created;
-	Date lastConnection;
-
-	public UserBo() {
-	}
-
-	public UserBo(int id, String login, Date created, Date lastConnection) {
-		super();
-		this.id = id;
-		this.login = login;
-		this.created = created;
-		this.lastConnection = lastConnection;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getLogin() {
-		return login;
-	}
-
-	public void setLogin(String login) {
-		this.login = login;
-	}
-
-	public Date getCreated() {
-		return created;
-	}
-
-	public void setCreated(Date created) {
-		this.created = created;
-	}
-
-	public Date getLastConnection() {
-		return lastConnection;
-	}
-
-	public void setLastConnection(Date lastConnection) {
-		this.lastConnection = lastConnection;
-	}
+	private int id;
+	private String login;
+	private Date created;
+	private Date lastConnection;
 
 }
--- root/src/main/java/fr/pjthin/root/selma/UserDto.java
+++ root/src/main/java/fr/pjthin/root/selma/UserDto.java
@@ -2,37 +2,16 @@ package fr.pjthin.root.selma;
 
 import java.util.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
-	@Override
-	public String toString() {
-		return "UserDto [login=" + login + ", lastConnection=" + lastConnection + "]";
-	}
 
-	String login;
-	Date lastConnection;
+    private String login;
+    private Date lastConnection;
 
-	public UserDto() {
-	}
-
-	public UserDto(String login, Date lastConnection) {
-		super();
-		this.login = login;
-		this.lastConnection = lastConnection;
-	}
-
-	public String getLogin() {
-		return login;
-	}
-
-	public void setLogin(String login) {
-		this.login = login;
-	}
-
-	public Date getLastConnection() {
-		return lastConnection;
-	}
-
-	public void setLastConnection(Date lastConnection) {
-		this.lastConnection = lastConnection;
-	}
 }
